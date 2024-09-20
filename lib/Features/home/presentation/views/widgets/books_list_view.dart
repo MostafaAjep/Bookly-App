8eebde9189b7f31d19df31ae16db5b675f872ccb
mostafa <Mostafaajep@gmail.com.com>
2024-09-20 18:22:58 +0300

    create styles file and add title home view

---
+++ lib/Features/home/presentation/views/widgets/books_list_view.dart
@@ -0,0 +1,22 @@
+import 'package:bokkly_app/Features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class BooksListView extends StatelessWidget {
+  const BooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        scrollDirection: Axis.horizontal,
+        physics: const BouncingScrollPhysics(),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const CustomListViewItem();
+        },
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
+import 'package:bokkly_app/Features/home/presentation/views/widgets/books_list_view.dart';
 import 'package:bokkly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bokkly_app/Features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bokkly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,30 +9,16 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
         BooksListView(),
+        SizedBox(height: 35),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Text('Best Seller', style: Styles.titleMedium),
+        ),
       ],
-    );
-  }
-}
-
-class BooksListView extends StatelessWidget {
-  const BooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        scrollDirection: Axis.horizontal,
-        physics: const BouncingScrollPhysics(),
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return const CustomListViewItem();
-        },
-      ),
     );
   }
 }
