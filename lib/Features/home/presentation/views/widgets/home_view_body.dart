3eeb104bc92fcc9463971d983b7e9efac7ebf00f
mostafa <Mostafaajep@gmail.com.com>
2024-09-20 19:20:29 +0300

    add BestSeller List View Item

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bokkly_app/Features/home/presentation/views/widgets/books_list_view.dart';
 import 'package:bokkly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bokkly_app/core/utils/assets.dart';
 import 'package:bokkly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -15,10 +16,42 @@ class HomeViewBody extends StatelessWidget {
         BooksListView(),
         SizedBox(height: 35),
         Padding(
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.all(14),
           child: Text('Best Seller', style: Styles.titleMedium),
         ),
+        BestSellerListViewItem(),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.8 / 4,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  // todo
+                  color: Colors.red,
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsData.testImage),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bokkly_app/Features/splash/presentation/views/splash_view.dart';
 import 'package:bokkly_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -19,6 +20,7 @@ class BooklyApp extends StatelessWidget {
         useMaterial3: false,
         brightness: Brightness.dark,
         scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       home: const SplashView(),
     );
